7db632a34ac969a01a911fd056b27da8fe13cf95
baumiger <167179467+baumiger@users.noreply.github.com>
2024-04-16 10:33:37 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,12 @@
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
 	
 })
+input.onButtonEvent(Button.AB, input.buttonEventClick(), function () {
+	
+})
+input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+	
+})
 let o1 = game.createSprite(0, 0)
 basic.forever(function () {
 	
